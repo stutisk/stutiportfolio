c8848e1d8fd08a6eb1816e5ee761ba12e8cf20cf
stutisk <48529714+stutisk@users.noreply.github.com>
2025-01-07 19:58:00 +0530

    initial

--- src/Components/index.js
+++
@@ -1,4 +0,0 @@
-import { Button } from "./Button/Button";
-import { Navbar } from "./Navbar/Navbar";
-import { TrendingComponents } from "./TrendingComponents/TrendingComponents";
-export {Button,TrendingComponents,Navbar };
--- src/Services/apiUrls.jsx
+++
@@ -1,6 +0,0 @@
-
-const BASE_URL = 'https://api.coingecko.com/api/v3';
-
-
-export const TRENDING_URL = `${BASE_URL}/search/trending`;
-export const PRICEOFBITCOIN_URL =`${BASE_URL}/simple/price`;
